69ac980893e2f51e6aa161e77c9dac0e3faa0ce6
Hesham El-Sawaf <heshamlsawaf@gmail.com>
2017-04-23 00:26:18 +0200

    Plotting method now takes a function as an argument.

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
+import numpy as np
+from PyQt4 import QtGui
 from PyQt4.uic import loadUiType
-
-from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt4agg import (
     FigureCanvasQTAgg as FigureCanvas,
     NavigationToolbar2QT as NavigationToolbar)
+from matplotlib.figure import Figure
+from sympy import *
 
 Ui_MainWindow, QMainWindow = loadUiType('window.ui')
+
+
+def f(x):
+    return np.sin(x);
+
 
 class Main(QMainWindow, Ui_MainWindow):
     def __init__(self, ):
         super(Main, self).__init__()
         self.setupUi(self)
+        self.fig1 = Figure()
 
-    def addmpl(self, fig):
+    def drawFig(self, fig):
         self.canvas = FigureCanvas(fig)
         self.mplvl.addWidget(self.canvas)
         self.canvas.draw()
@@ -20,19 +28,22 @@ class Main(QMainWindow, Ui_MainWindow):
                                          self.mplwindow, coordinates=True)
         self.mplvl.addWidget(self.toolbar)
 
-if __name__ == '__main__':
-    import sys
-    from PyQt4 import QtGui
-    import numpy as np
+    def plot(self, y):
+        plt = self.fig1.add_subplot(111)
+        xs = np.arange(-100.0, 100.0, 0.1)
+        plt.plot(xs, y(xs))
+        plt.axis([-6, 6, -1, 1])
+        self.drawFig(self.fig1)
 
+
+if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
     main = Main()
     main.show()
 
-    #figure example
-    fig1 = Figure()
-    ax1f1 = fig1.add_subplot(111)
-    ax1f1.plot(np.random.rand(5))
-    main.addmpl(fig1)
+    # figure example
+    x = Symbol('x')
+    y = sin(x)
+    main.plot(lambdify(x, y.diff(x), 'numpy'))
 
     sys.exit(app.exec_())
