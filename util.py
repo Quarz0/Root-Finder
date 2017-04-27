a9b869803f3997ab9287468326b6d00730019eb4
Marwan Tammam <marwan.01010100@gmail.com>
2017-04-27 16:53:31 +0200

    Add bisection method and util functions

---
+++ util.py
@@ -0,0 +1,28 @@
+import math, sympy, sympy.parsing.sympy_parser
+
+global X
+X = sympy.Symbol('x')
+
+def calcPrecision(ea):
+    return int(2.0 - math.log10(2.0 * ea))
+
+def evaluateFunc(f, val):
+    return sympy.N(f.subs({X: val}))
+
+def evaluateNthDerivative(f, val, n):
+    return sympy.N(sympy.diff(f, X, n).subs({X: val}))
+
+def parseExpr(expr):
+    modifiedExpr = ""
+    for c in expr.lower().replace('^', '**'):
+        if c >= 'a' and c <= 'z':
+            if len(modifiedExpr) > 0 and modifiedExpr[-1] >= '0' and modifiedExpr[-1] <= '9':
+                modifiedExpr += '*'
+            if c == 'e':
+                modifiedExpr += "E"
+            else:
+                modifiedExpr += c
+        else:
+            modifiedExpr += c
+    return sympy.parsing.sympy_parser.parse_expr(modifiedExpr)
+
