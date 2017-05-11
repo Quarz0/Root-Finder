f584567e42ab22cb5f3bd5ed73acdf458df02a8d
Marwan Tammam <marwan.01010100@gmail.com>
2017-05-11 10:42:29 +0200

    Modified to match resultset and return equation objects, and boundaries for all iterations rather than first one only

--- bisection.py
+++ bisection.py
@@ -3,18 +3,20 @@ import timeit
 from resultset import ResultSet
 from table import Table
 from util import *
+from equation import equation
 
 
 def bisection(func, xl, xu, iterations=50, eps=0.00001):
     if evaluateFunc(func, xl) * evaluateFunc(func, xu) > 0:
         return float('nan')
 
-    boundaryLines = [xl, xu]
+    boundaryLines = []
     iterationRows = []
     startTime = timeit.default_timer()
     xr_old = None
 
     for i in xrange(iterations):
+        boundaryLines.append([equation(xl, True), equation(xu, True)])
         xr = (xu + xl) / 2
 
         if xr_old != None:
@@ -38,8 +40,8 @@ def bisection(func, xl, xu, iterations=50, eps=0.00001):
     executionTime = timeit.default_timer() - startTime
     table = Table("Bisection", ['Step', 'xl', 'f(xl)', 'xu', 'f(xu)', 'xr', 'Ea (%)'], iterationRows)
 
-    return ResultSet(table, xr, calcPrecision(ea), executionTime, i, [sympy.lambdify('x', func, 'numpy')],
-                     vLines=boundaryLines)
+    return ResultSet(table, xr, calcPrecision(ea), executionTime, i, [equation(sympy.lambdify('x', func, 'numpy'))],
+                     boundaries=boundaryLines)
 
 
 # Test
@@ -48,8 +50,8 @@ def f(x):
     return x ** 3 - 0.165 * x ** 2 + 3.993 * 10 ** -4
 
 
-if __name__ == '__main__':
-    str = 'x^3 - 0.165x^2 + 10^-4'
-    expr = parseExpr(str)
+# if __name__ == '__main__':
+str = 'x^3 - 0.165x^2 + 10^-4'
+expr = parseExpr(str)
 
-    print bisection(0.00, 0.11, expr)
+print bisection(expr, 0.00, 0.11)
